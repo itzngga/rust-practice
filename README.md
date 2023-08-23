32c55833b0353d34420cc3e671e1bf27e6eac5ea
itzngga <rangganak094@gmail.com>
2023-08-23 12:05:34 +0700

    feat: init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Rust Programming Practice
+### my journey to low-level programming languages
+
+## [Playlist Link](https://www.youtube.com/playlist?list=PLIfsrcorUur10nUSHVq9Mb4yWu87_5lZn)
---
+++ common/src/main.rs
@@ -0,0 +1,82 @@
+fn main() {
+    // let a = 10;
+    // let mut b = 20;
+    //
+    // const HIGHEST_PRICE: u32 = 10_000_000;
+    //
+    // let x = 10;
+    // let x = "ten";
+    //
+    // let y: u32 = 10;
+    //
+    // let f: f64 = 2.1;
+    // let b: bool = true;
+    //
+    // let c = 'c';
+    //
+    // let tup: (i32, f64, u8) = (100, 1.3, 1);
+    // let (x, y, z) = tup;
+    //
+    // let first = tup.0;
+    //
+    // let tup2: () = ();
+
+    // let a: [i32; 3] = [1, 2, 3];
+    //
+    // let b: [i32; 5] = [1, 2, 3, 4, 5];
+    // let b = [3, 5];
+    // let first = b[0];
+
+    // println!("hello rust");
+    // my_func(3, 'h');
+    // let val = add_int(10, 12);
+    // println!("{}", val);
+    //
+    // let num = 3;
+    // if num <= 5 {
+    //     println!("lower or equal than 5 {}", num);
+    // } else {
+    //     println!("greater than 5");
+    // }
+    //
+    // let num2 = if num == 3 { 5 } else { 6 };
+
+    // loop {
+    //     println!("loop...");
+    // }
+
+    // let mut counter = 0;
+    // let res = loop {
+    //     counter += 1;
+    //
+    //     if counter == 10 {
+    //         break counter * 2;
+    //     }
+    // };
+    //
+    // println!("{}", res);
+
+    // let mut num = 3;
+    //
+    // while num != 0 {
+    //     println!("{}", num);
+    //     num -= 1;
+    // }
+    //
+    // println!("bye");
+
+    let a = [1, 2, 3];
+
+    for i in a {
+        println!("{}", i);
+    }
+}
+
+// fn my_func(value: i32, label: char) {
+//     println!("result {}{}", value, label);
+// }
+//
+// fn add_int(x: i32, y: i32) -> i32 {
+//     x + y
+// }
+
