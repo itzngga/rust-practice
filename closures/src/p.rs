15e1415f7f325bdc4931f9f6227ccfbf30774fdb
itzngga <rangganak094@gmail.com>
2023-12-08 16:25:58 +0700

    feat: closures

---
+++ closures/src/p.rs
@@ -0,0 +1,74 @@
+use std::thread;
+use std::time::Duration;
+
+mod p;
+
+// fn simulated_expensive_calculation(intensity: u32) -> u32 {
+//     println!("calculate slowly...");
+//     thread::sleep(Duration::from_secs(2));
+//     intensity
+// }
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cache::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes",
+                expensive_result.value(intensity)
+            );
+        }
+    }
+}
+
+struct Cache<T>
+where
+    T: Fn(u32) -> u32,
+{
+    calculation: T,
+    value: Option<u32>,
+}
+
+impl<T> Cache<T>
+where
+    T: Fn(u32) -> u32,
+{
+    fn new(calculation: T) -> Cache<T> {
+        Cache {
+            calculation,
+            value: None,
+        }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
+}
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+
+    // let example_closure = |x| x;
+    // let s = example_closure(String::from("hello"));
+    // let n = example_closure(5);
+}
